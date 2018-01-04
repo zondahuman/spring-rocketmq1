eaed04a32245f30616ba1df330c1ce7205e76467
zondahuman <zondahuman@gmail.com>
2018-01-04 14:16:06 +0800

    update

--- README.md
+++ README.md
@@ -2,22 +2,23 @@ march action
 
 # Rocketmq Note:
 
+ using method:
 
 
+# Broker
+nohup sh mqbroker -n 172.16.2.133:9876 autoCreateTopicEnable=true > ~/logs/rocketmqlogs/broker.log 2>&1 &
 
+# NameSvr
+nohup mqnamesrv 1>/opt/alibaba-rocketmq/log/ng.log 2>/opt/alibaba-rocketmq/log/ng-err.log &
 
+# WebConsole
+https://github.com/apache/rocketmq-externals.git
+cd rocketmq-externals
+mvn clean install -Dmaven.test.skip=true -U
 
+java -jar rocketmq-console-ng-1.0.0.jar --server.port=12581 --rocketmq.config.namesrvAddr=172.16.2.133:9876
 
-
-
-
-
-
-
-
-
-
-
+http://172.16.2.133:12581
 
 
 
--- spring-rocketmq1-skeleton/src/main/java/com/abin/lee/spring/rocketmq/skeleton/consumer/RocketmqConsumer.java
+++ spring-rocketmq1-skeleton/src/main/java/com/abin/lee/spring/rocketmq/skeleton/consumer/RocketmqConsumer.java
@@ -1,16 +1,16 @@
 package com.abin.lee.spring.rocketmq.skeleton.consumer;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.consumer.listener.*;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.MixAll;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by abin on 2018/1/3 19:02.
@@ -20,18 +20,34 @@ import java.util.List;
 public class RocketmqConsumer {
 
     public static void main(String[] args) throws InterruptedException, MQClientException, IOException {
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("SPEED.QUEUE");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("SPEED_QUEUE");
+
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        consumer.subscribe("TopicTest", "TagA || TagC || TagD");
         consumer.setNamesrvAddr("172.16.2.133:9876");
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
+        consumer.registerMessageListener(new MessageListenerOrderly() {
 
-        consumer.registerMessageListener(new MessageListenerConcurrently() {
-
+            AtomicLong consumeTimes = new AtomicLong(0);
             @Override
-            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
-                                                            ConsumeConcurrentlyContext context) {
-                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
-                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs,
+                                                       ConsumeOrderlyContext context) {
+                context.setAutoCommit(false);
+                System.out.printf(Thread.currentThread().getName() + " Receive New Messages: " + msgs + "%n");
+                this.consumeTimes.incrementAndGet();
+                if ((this.consumeTimes.get() % 2) == 0) {
+                    return ConsumeOrderlyStatus.SUCCESS;
+                } else if ((this.consumeTimes.get() % 3) == 0) {
+                    return ConsumeOrderlyStatus.ROLLBACK;
+                } else if ((this.consumeTimes.get() % 4) == 0) {
+                    return ConsumeOrderlyStatus.COMMIT;
+                } else if ((this.consumeTimes.get() % 5) == 0) {
+                    context.setSuspendCurrentQueueTimeMillis(3000);
+                    return ConsumeOrderlyStatus.SUSPEND_CURRENT_QUEUE_A_MOMENT;
+                }
+                return ConsumeOrderlyStatus.SUCCESS;
+
             }
         });
 
